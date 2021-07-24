f169289814e449bb87238c1d1be1e41361e76dbc
simorgh10 <issam.benabid@gmail.com>
2021-07-24 19:07:27 +0200

    Personalize Button component

--- src/app/components/button/button.component.ts
+++ src/app/components/button/button.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-button',
@@ -6,10 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./button.component.css']
 })
 export class ButtonComponent implements OnInit {
+  @Input() color: string = "";
+  @Input() text: string = "";
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+  
+  onClick(): void {
+    console.log('Add');
   }
 
 }
